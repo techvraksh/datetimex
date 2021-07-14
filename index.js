859237152c9eeb044c220c1bc2981064074447af
Srashti Jain <srashti0204@gmail.com>
2021-07-14 10:08:53 +0530

    Added getFullMonth function

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+ getFullMonth:(date) =>{
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  return monthNames[date.getMonth()]
+}
+}
